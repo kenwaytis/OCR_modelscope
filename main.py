4f2819d5b0e91b10a7bc9b76d8a5087c6e8d3f54
kenwaytis <kenwaytis@163.com>
2023-07-14 12:01:43 +0800

    fix no result

--- main.py
+++ main.py
@@ -28,6 +28,20 @@ async def prediction(items:Image):
             img_binary = base64.b64decode(img_b64)
             buffer = np.frombuffer(img_binary,dtype=np.uint8)
             img = cv2.imdecode(buffer,flags=cv2.IMREAD_COLOR)
+
+
+            # 图片短边小于100时，按原比例放到到短边为100
+            height, width = img.shape[:2]
+            if min(height, width) < 100:
+                if height < width:
+                    new_height = 100
+                    new_width = int(width * new_height / height)
+                else:
+                    new_width = 100
+                    new_height = int(height * new_width / width)
+                logger.info("resize img")
+                img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
+
             det_result = ocr_detection(img)
             logger.debug(f"det_result: {det_result}")
             det_result = det_result['polygons'] 
@@ -56,10 +70,11 @@ async def prediction(items:Image):
         logger.info(results)
         return final_result
     except Exception as e:
+        logger.error(e)
         res = {
-            "msg": e, 
-            "results": "",
-            "status": "001"
+            "msg": str(e), 
+            "results": ["no text found"],
+            "status": "000"
         }
         return res
             
